c5f862ffd02e2f5405569ee24706f45530328991
Yahya Bin Naveed <yahya.bin.naveed@gmail.com>
2023-03-16 11:40:03 +0500

    Initial commit

---
+++ lib/reg.dart
@@ -0,0 +1,11 @@
+import 'package:lab6/main.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+} 
